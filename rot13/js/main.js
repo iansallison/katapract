2227c64aa153223726e1ca08fcf809a5e9e21439
Ian Allison <iansallison@gmail.com>
2022-08-30 11:01:12 -0600

    added rot13

---
+++ rot13/js/main.js
@@ -0,0 +1,5 @@
+function rot13(message){
+  const alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const ciph = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m'];
+  return message.split('').map(e => alpha.includes(e)? ciph[alpha.indexOf(e)]: e).join('');
+}
